1851952269b36928b49a6672182434686ca65768
DanikTL <danik4tl@gmail.com>
2020-10-08 15:58:15 +0200

    object

--- histfile/__init__.py
+++ histfile/__init__.py
@@ -4,6 +4,92 @@ from matplotlib import pyplot
 files_ls = []
 
 
+# using object
+
+class CustomHistData:
+
+    def __init__(self, array=None):
+        self.array = array
+
+    def update_array(self, array: np.ndarray):
+        """
+        Update n-d array from the numpy object
+        :param array:
+        :return:
+        """
+        self.array = array
+
+    def upload_data(self, path: str, separator="\t", as_int=False, as_float=False):
+        """
+        Upload n-d array from the file (with conversion to the numpy array)
+        :param path: Absolute file path
+        :param separator: columns separator
+        :param as_int: convert array to array of ints
+        :param as_float: convert array to array of floats
+        :return: None
+        """
+        with open(path, "r") as file:
+            data = file.read()
+            data_lines = data.splitlines()
+            if not len(data_lines):
+                return np.asarray([])
+            data_f_line = data_lines[0]
+            if not len(data_f_line):
+                return np.asarray([])
+            cols = len(data_f_line.split(separator))
+            result = init_array(cols)
+
+            for row in range(len(data_lines)):
+                data_row = data_lines[row].split(separator)
+                for idx in range(min(len(data_row), cols)):
+                    result[idx].append(data_row[idx])
+
+            result = np.asarray(result)
+
+            try:
+                if as_int:
+                    try:
+                        result = result.astype(np.int)
+                    except ValueError:
+                        result = result.astype(np.float)
+                        result = result.astype(np.int)
+                elif as_float:
+                    result = result.astype(np.float)
+            except ValueError:
+                pass
+        self.array = result
+
+    def draw_plots(self, as_prob=False):
+        """
+            Method to draw plots (bar for each value)
+            :param as_prob: draw plot in probability scale
+            :return: None
+            """
+        for column in self.array:
+            uniq, counts = np.unique(column, return_counts=True)
+            ### debug
+            # print(dict(zip(uniq, counts)))
+            weights = np.ones(len(column))
+            if as_prob:
+                weights = np.ones(len(column)) / len(column)
+            pyplot.hist(column, alpha=0.5, bins=len(uniq), weights=weights)
+            pyplot.show()
+
+    def get_min(self, by_row=False):
+        if by_row:
+            return np.amin(self.array, axis=0)
+        else:
+            return np.amin(self.array, axis=1)
+
+    def get_max(self, by_row=False):
+        if by_row:
+            return np.amax(self.array, axis=0)
+        else:
+            return np.amax(self.array, axis=1)
+
+
+# functions
+
 def init_array(cols=2) -> list:
     """
     Help function to generate empty n-d array
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import histfile
 
 
-def example():
+def examples():
+    # Example 1
     filename = "disc.dat"
     array_ = histfile.array_from_file(f"data/{filename}", as_float=True)
     maxs = histfile.find_max_by_cols(array_)
@@ -9,7 +10,14 @@ def example():
     print(maxs, mins)
     for col in range(len(array_)):
         histfile.make_hist(array_[col], as_prob=True)
+    # Example 2
+    hist_data = histfile.CustomHistData()
+    hist_data.upload_data(path=f'data/{filename}', as_float=True)
+    maxs = hist_data.get_max()
+    mins = hist_data.get_min()
+    print(maxs, mins)
+    hist_data.draw_plots()
 
 
 if __name__ == '__main__':
-    example()
+    examples()
